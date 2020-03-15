04c033aec0ec1cc740deb6929d5355fa7a79b960
ThomasvLingen <thovali96@gmail.com>
2020-03-15 20:31:13 +0100

    chore: improve readability on nrf_sdk.mk
    Removed boilerplate macro, no reason to separate it.
    Added space between preproc definitions, they're hard enough to read
    with them as is, not having a space just makes it worse.

--- nrf_sdk.mk
+++ nrf_sdk.mk
@@ -3,18 +3,11 @@ NRF_LIB = $(NRF)/components/libraries
 NRF_X = $(NRF)/modules/nrfx
 NRF_EXTERNAL = $(NRF)/external
 
-BOILERPLATE_INCLUDE = \
+
+INCLUDE_PATHS += \
 	-I $(NRF)/components/boards \
 	-I $(NRF)/components/toolchain/cmsis/include \
 	-I $(NRF)/components/drivers_nrf/nrf_soc_nosd \
-
-
-BOILERPLATE_SRC_FILES = \
-	$(NRF_X)/mdk/system_nrf52840.c \
-	$(NRF)/components/boards/boards.c \
-
-INCLUDE_PATHS += \
-	$(BOILERPLATE_INCLUDE) \
 	-I $(NRF_X) \
 	-I $(NRF_X)/mdk \
 	-I $(NRF_X)/hal \
@@ -23,23 +16,25 @@ INCLUDE_PATHS += \
 	-I $(NRF_LIB)/util \
 	-I $(NRF_EXTERNAL)/segger_rtt \
 
+
 SRC_PATH += \
 	$(NRF_EXTERNAL)/segger_rtt \
 
 SRC_FILES += \
-	$(BOILERPLATE_SRC_FILES) \
+	$(NRF_X)/mdk/system_nrf52840.c \
+	$(NRF)/components/boards/boards.c \
 	$(NRF_LIB)/util/app_util_platform.c \
 
 AS_FILES += \
 	$(NRF_X)/mdk/gcc_startup_nrf52840.S
 
 COMPILER_FLAGS += \
-	-DUSE_APP_CONFIG \
-	-DBOARD_PCA10056 \
-	-DBSP_DEFINES_ONLY \
-	-DCONFIG_GPIO_AS_PINRESET \
-	-DFLOAT_ABI_HARD \
-	-DNRF52840_XXAA \
+	-D USE_APP_CONFIG \
+	-D BOARD_PCA10056 \
+	-D BSP_DEFINES_ONLY \
+	-D CONFIG_GPIO_AS_PINRESET \
+	-D FLOAT_ABI_HARD \
+	-D NRF52840_XXAA \
 	-mcpu=cortex-m4 \
 	-mthumb -mabi=aapcs \
 	-mfloat-abi=hard -mfpu=fpv4-sp-d16 \
@@ -53,7 +48,8 @@ LINKER_FLAGS += \
 	-mthumb -mabi=aapcs \
 	-mcpu=cortex-m4 \
 	-mfloat-abi=hard -mfpu=fpv4-sp-d16 \
-	-lm -lc -lgcc -lnosys \
+	-l m -l c -l gcc -l nosys \
+
 
 OUT_COMBINED_HEX = $(OUT_HEX:.hex=-combined.hex)
 SOFTDEVICE_HEX = $(NRF)/components/softdevice/s140/hex/s140_nrf52_7.0.1_softdevice.hex
