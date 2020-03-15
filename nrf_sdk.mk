d0be21aeb29af41bdbe861801cb129524ee343df
ThomasvLingen <thovali96@gmail.com>
2020-03-15 15:49:21 +0100

    chore: remove nrf sdk boilerplate

--- nrf_sdk.mk
+++ nrf_sdk.mk
@@ -12,7 +12,6 @@ BOILERPLATE_SRC_FILES = \
 	$(NRF_X)/mdk/system_nrf52840.c \
 	$(NRF)/components/boards/boards.c \
 
-
 INCLUDE_PATHS += \
 	$(BOILERPLATE_INCLUDE) \
 	-I $(NRF_X) \
@@ -21,31 +20,10 @@ INCLUDE_PATHS += \
 	-I $(NRF)/integration/nrfx \
 	-I $(NRF_LIB)/delay \
 	-I $(NRF_LIB)/util \
-	-I $(NRF_LIB)/log \
-	-I $(NRF_LIB)/log/src \
-	-I $(NRF_LIB)/experimental_section_vars \
-	-I $(NRF_LIB)/strerror \
-	-I $(NRF_LIB)/balloc \
-	-I $(NRF_LIB)/atomic \
 
 
 SRC_FILES += \
 	$(BOILERPLATE_SRC_FILES) \
-	$(NRF)/external/fprintf/nrf_fprintf.c \
-	$(NRF)/external/fprintf/nrf_fprintf_format.c \
-	$(NRF_X)/soc/nrfx_atomic.c \
-	$(NRF_LIB)/log/src/nrf_log_frontend.c \
-	$(NRF_LIB)/log/src/nrf_log_str_formatter.c \
-	$(NRF_LIB)/util/app_error.c \
-	$(NRF_LIB)/util/app_error_handler_gcc.c \
-	$(NRF_LIB)/util/app_error_weak.c \
-	$(NRF_LIB)/util/app_util_platform.c \
-	$(NRF_LIB)/util/nrf_assert.c \
-	$(NRF_LIB)/atomic/nrf_atomic.c \
-	$(NRF_LIB)/balloc/nrf_balloc.c \
-	$(NRF_LIB)/memobj/nrf_memobj.c \
-	$(NRF_LIB)/ringbuf/nrf_ringbuf.c \
-	$(NRF_LIB)/strerror/nrf_strerror.c \
 
 AS_FILES += \
 	$(NRF_X)/mdk/gcc_startup_nrf52840.S
